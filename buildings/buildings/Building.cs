1ea8fc2da51d4be1b2ed10f62eccd865518e1390
HitarthV <129500306+HitarthV@users.noreply.github.com>
2023-09-20 11:31:24 +0100

    Add files via upload

---
+++ buildings/buildings/Building.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildings
+{
+    public class Building
+    {
+            protected int pFloors;
+            protected int pWidth;
+            protected int pHeight;
+
+            public Building(int numberFloors, int width, int height)
+            {
+                pFloors = numberFloors;
+                pWidth = width;
+                pHeight = height;
+            }
+            public int getNumberFloors()
+            {
+                return pFloors;
+            }
+            public int getWidth()
+            {
+                return pWidth;
+            }
+            public int getHeight()
+            {
+                return pHeight;
+            }
+            public void setNumberFloors(int numberFloors)
+            {
+                if (numberFloors > 0)
+                {
+                    this.pFloors = numberFloors;
+                }
+                else
+                {
+                    Console.WriteLine("Number of floors must be greater than 0");
+                }
+            }
+            public void setWidth(int width)
+            {
+                if (width > 0)
+                {
+                    this.pWidth = width;
+                }
+                else
+                {
+                    Console.WriteLine("Width must be greater than 0");
+                }
+            }
+            public void setHeight(int height)
+            {
+                if (height > 0)
+                {
+                    this.pHeight = height;
+                }
+                else
+                {
+                    Console.WriteLine("Height must be greater than 0");
+                }
+            }
+    }
+}
---
+++ buildings/buildings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace buildings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building mybuilding = new Building(2,200,300);
+            Console.WriteLine(mybuilding.getWidth());
+            Console.WriteLine(mybuilding.getHeight());
+            Console.WriteLine(mybuilding.getNumberFloors());
+        }
+    }
+}
